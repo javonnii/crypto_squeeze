bb87cfd40b24e9435f7a155cbcab6f8aab044fed
Javonnii Curry <javonniicurry@gmail.com>
2021-06-03 17:36:47 -0700

    Auto Update: Thu 17:36 Jun 03 2021

---
+++ crypto_squeeze_app.py
@@ -0,0 +1,9 @@
+import os 
+import pandas as pd 
+from posix import listdir 
+import plotly.graph_objects as go 
+import streamlit as st 
+
+st.title("Top 25 Crypto Marketcap Squeeze Scanner")
+
+st.subheader("By Javonnii Curry")
